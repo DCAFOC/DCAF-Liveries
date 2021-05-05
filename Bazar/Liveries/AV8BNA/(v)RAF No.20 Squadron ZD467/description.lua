27b105afd2572abbb66a1aa7c219d7a67f005d58
CTCGraphics <34329447+CTCGraphics@users.noreply.github.com>
2021-05-05 23:24:31 +0100

    2.0.0
    AV8BNA
    -(v)RAF No.4 Squadron ZD401-412
    	-(v)RAF No.20 Squadron
    >ZD464 Adam “Princess” Kelan Remove rank, add teeth
    Killmarkings: 1 x Mig-31 Mixed ordnance
    >ZD465 Ben “Freefall” Edward
    >ZD466 Harry “Geneva” Lee
    		Killmarkings: 1 x Mig-29
    >ZD467 Fritz "Tuba" Leineweber (MD)
    	Killmarkings: 5 x GBU-12s
    >ZD468 Rozza “Sonic” Donachie
    >ZD469 Tom “Davinci” Painter
    >ZD470 Lloyd “Buzz” Benson
    >ZD471 Paul “Nemo” Dodge
    >ZD472 John “Leprechaun” McIntyre
    >ZD473 Andrew “Mower” Edward
    >ZD474 Kev “Drop-Top” Gossage
    
    FA-18C_hornet
    	-(v)RAAF No. 2 OCU
    		Roughmatte registrations
    fix tail alignment
    	-(v)USMC VMFA-115 ‘Silver Eagles’
    -(v)USN VFA-103 'Jolly Rogers'
    Fix helmets
    New colours from AG-200
    	>AG-200 Divano “Shveaty” Jongsma TBD
    		>Bort numbers for Nose and Tail
    	>AG-201 Ahsan “Prettyboy” Sajjad
    	>AG-212 Killian “Brick” Brand
    	>AG-204 Jacob “Nerf” Thomas
    	>AG-205 Michael “Poseidon” Hall
    	>AG-211 Joop “Pigeon” Roukens
    	>AG-206 PK “Iceman” Bergheim
    
    	TF-51D
    -(v)RCAF 402 Squadron
    
    	F-14A
    >Recreate liveries, remove ranks and create individual registrations MOVE TO A folder
    -(v)USN VF-154 'Black Knights'
    	>NF-100 Rob “Hawkeye” Klein / RIO?
    		>NF-102 Mathew “Cowboy” Kilburg RIO?
    >NF-103 Matthew "Moose" Muzdeka
    >NF-104 Turo “Casper” Oraste / RIO?
    >NF-105 John "TBD" Springer
    -(v)USN VF-124 'Gunfighters'
    
    F-16C_50
    (v)RNLAF 323rd Squadron 1981-1992
    	Reverse 323 patch
    	>J-200 Chris “Posh” Callow
    	(v)RNLAF 323rd Squadron 1992-2014
    		>J-201 Joris “Joe” Roggekamp
    		>J-205 Mike “Soap” Berckmannetje
    	(v)USAF 119th Fighter Squadron 1995-2011
    (v)USAF 119th Fighter Squadron 2011+
    (v)USAF 119th Fighter Squadron AC-333
    (v)USAF 312th Tactical Fighter Training Squadron
    (v)USAF 312th Tactical Fighter Training Squadron CO
    	>Fix helmet

---
+++ Bazar/Liveries/AV8BNA/(v)RAF No.20 Squadron ZD467/description.lua
@@ -0,0 +1,60 @@
+livery = {
+
+	{"AV8B1", 0 ,"AV8_T1",false};
+	{"AV8B2", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T2",false};
+	{"AV8B3", 0 ,"AV8_T3",false};
+	{"AV8B4", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T4",false};
+	{"AV8B7", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T7",false};
+	{"AV8B8", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T8",false};
+	
+	{"pilot_AV8B_body", 0 ,"pilot_AV8B_G",true};
+	{"pilot_AV8B_helmet", 0 ,"../(v)RAF No.20 Squadron ZD464/pilot_av8b_hgu68", false};
+	{"pilot_AV8B_helmet", DECAL ,"../(v)RAF No.4 Squadron ZD401/pilot_av8b_hgu68_decal", false};
+	{"pilot_AV8B_patch", 0 ,"../(v)RAF No.20 Squadron ZD464/pilot_AV8B_patch_Generic",false};
+	
+	{"AV8B_T1_FUSE1", 0 ,"AV8_T1",false};
+	{"AV8B_T1_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T1_FUSE10", 0 ,"AV8_T1",false};
+	{"AV8B_T1_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE1", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T2",false};
+	{"AV8B_T2_FUSE1", DECAL ,"empty",true};
+	
+	{"AV8B_T2_FUSE10", 0 ,"../(v)RAF No.20 Squadron ZD464/AV8_T2",false};
+	{"AV8B_T2_FUSE10", DECAL ,"empty",true};
+	
+	{"AV8B_T3_FUSE1", 0 ,"AV8_T3",false};
+	{"AV8B_T3_FUSE1", DECAL ,"empty",true};
+	{"AV8B_T3_FUSE1", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	
+	{"AV8B_T3_FUSE10", 0 ,"AV8_T3",false};
+	{"AV8B_T3_FUSE10", DECAL ,"empty",true};
+	{"AV8B_T3_FUSE10", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	
+	{"AV8B_T3_TAIL1", 0 ,"AV8_T3",false};
+	{"AV8B_T3_TAIL1", DECAL ,"empty",true};
+	{"AV8B_T3_TAIL1", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	
+	{"AV8B_T3_TAIL10", 0 ,"AV8_T3",false};
+	{"AV8B_T3_TAIL10", DECAL ,"empty",true};
+	{"AV8B_T3_TAIL10", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	
+	{"AV8B_T3_TAIL100", 0 ,"AV8_T3",false};
+	{"AV8B_T3_TAIL100", DECAL ,"empty",true};
+	{"AV8B_T3_TAIL100", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	
+	{"AV8B1", ROUGHNESS_METALLIC ,"AV8_T1_RoughMet",true};
+	{"AV8B2", ROUGHNESS_METALLIC ,"AV8_T2_RoughMet",true};
+	{"AV8B3", ROUGHNESS_METALLIC ,"../(v)RAF No.20 Squadron ZD464/AV8_T3_RoughMet",false};
+	{"AV8B4", ROUGHNESS_METALLIC ,"../(v)RAF No.4 Squadron ZD401/AV8_T4_RoughMet",false};
+	{"AV8B7", ROUGHNESS_METALLIC ,"AV8_T7_RoughMet",true};
+	{"AV8B8", ROUGHNESS_METALLIC ,"AV8_T8_RoughMet",true};
+
+}
+
+name = "(v)RAF No.20 Squadron ZD467"
+--countries = {"UK"}
+--Property of Digital Coalition Airforce Skinpack
+--DCAF by CTCVisuals 19/10/2018
+--https://www.fiverr.com/ctcvisuals/create-custom-dcs-world-liveries
---
+++ CoreMods/aircraft/F14/Liveries/F-14A-135-GR/VF-124 Gunfighters (1975)/description.lua
@@ -0,0 +1,32 @@
+livery = {
+
+
+--Diffuse  -----------------------------------
+
+{"HB_F14_EXT_01", 0 ,"HB_F14_EXT_01",false};
+{"HB_F14_EXT_02", 0 ,"HB_F14_EXT_02",false};
+{"HB_F14_EXT_03", 0 ,"HB_F14_EXT_03",false};
+{"HB_F14_EXT_04", 0 ,"HB_F14_EXT_04",false};
+{"HB_F14_EXT_TAIL", 0 ,"HB_F14_EXT_TAIL",false};
+{"HB_F14_WING_LEFT_01", 0 ,"HB_F14_WING_LEFT",false};
+{"HB_F14_WING_RIGHT", 0 ,"HB_F14_WING_RIGHT",false};
+{"HB_F14_EXT_PYLONS", 0 ,"HB_F14_EXT_PYLONS_01",false};
+{"HB_F14_EXT_DROPTANKS", 0 ,"HB_F14_EXT_DROPTANK",false};
+{"HB_F14_EXT_PHOENIXRAILS", 0 ,"HB_F14_EXT_PHOENIXPYLONS",false};
+{"HB_F14_TCS", 0 ,"HB_F14_EXT_TCS",false};
+--Pilot--
+{"HB_F14_EXT_PILOT_HELMET", 0 ,"HB_F14_EXT_PILOT_HELMET",false};
+{"HB_F14_EXT_PILOT_HELMET", 1 ,"HB_F14_EXT_PILOT_HELMET_Nrm",false};
+{"HB_F14_EXT_PILOT_HELMET", ROUGHNESS_METALLIC ,"HB_F14_EXT_PILOT_HELMET_RoughMet",false};
+
+--RIO--
+{"HB_F14_EXT_RIO_HELMET", 0 ,"HB_F14_EXT_PILOT_HELMET",false};
+{"HB_F14_EXT_RIO_HELMET", 1 ,"HB_F14_EXT_PILOT_HELMET_Nrm",false};
+{"HB_F14_EXT_RIO_HELMET", ROUGHNESS_METALLIC ,"HB_F14_EXT_PILOT_HELMET_RoughMet",false};
+
+{"HB_F14_LOD1_3in1", 0 ,"HB_F14_LOD1_3in1",false};
+{"HB_F14_EXT_PILOT_SUIT", 0 ,"HB_F14_EXT_PILOT_SUIT",false};
+}
+name = "VF-124 Gunfighters *NJ-401* (1975)"
+
+--countries = {"USA", "RUS", "FRA", "UKR", "SPN", "NETH", "TUR", "BEL", "GER", "NOR", "CAN", "DEN", "UK", "GRG", "ISR", "ABH", "RSO"}
