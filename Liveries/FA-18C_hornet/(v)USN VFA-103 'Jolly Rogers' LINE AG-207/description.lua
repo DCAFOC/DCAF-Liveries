a4ef55b55db2dd1f1ad031008ce91959066ab7bb
CTCGraphics <34329447+CTCGraphics@users.noreply.github.com>
2021-06-28 09:32:04 +0100

    VFA-103 Name changes
    >AG-202 Kilian “Brick” Brand
    >AG-204 Joris "Jugs" Roggekamp
    >AG-212 C “Lowrider” Pony
    >AG-207 Hanno “Kief” Ratief

---
+++ Liveries/FA-18C_hornet/(v)USN VFA-103 'Jolly Rogers' LINE AG-207/description.lua
@@ -0,0 +1,210 @@
+livery = {
+	--Main Maps
+	{"f18c1", 0 ,"F18C_1_DIFF",false};
+	{"f18c1", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+
+	{"f18c2", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"f18c2", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	
+	--Pilot Maps
+	{"pilot_F18_patch", 0 ,"../(v)USN VFA-103 'Jolly Rogers' CAG AG-200/Pilot_F18_Patch",false};
+	{"pilot_F18_patch", 1 ,"../(v)USN VFA-103 'Jolly Rogers' CAG AG-200/Pilot_F18_Patch_NORM",false};
+	{"pilot_F18_patch", ROUGHNESS_METALLIC ,"../(v)USN VFA-103 'Jolly Rogers' CAG AG-200/Pilot_F18_Patch_RoughMet",false};
+	{"pilot_F18_helmet", 0, "../(v)USN VFA-103 'Jolly Rogers' CAG AG-200/pilot_F18_helmet", false};
+	{"pilot_F18_helmet", 13, "pilot_F18_helmet_RoughMet", true};
+	
+	--Fuel Tanks
+	{"FPU_8A", 0 ,"../(v)USN VFA-103 'Jolly Rogers' FL/FPU_8A",false};
+	{"FPU_8A", ROUGHNESS_METALLIC ,"FPU_8A_Diff_RoughMet",true};
+	
+	--Stay / covers
+	{"f18c_stay", 0, "../(v)USN VFA-103 'Jolly Rogers' CAG AG-200/F18C_1_DIFF_STAY", false};
+    --{"f18c_stay", 1, "f18c_1_diff_stay_nm", true};
+    --{"f18c_stay", 2, "f18c_1_diff_stay_dif_roughmet", true};
+	
+	--Nose Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_L_100", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_L_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_NOSE_L_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_NOSE_L_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_L_01", DECAL ,"F18C_bort_number2",true};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_R_100", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_R_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_NOSE_R_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_NOSE_R_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_R_01", DECAL ,"F18C_bort_number2",true};	
+		
+	--LINEap Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_ZAK_L_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_ZAK_L_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_ZAK_L_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_L_01", DECAL ,"F18C_bort_number2",true};
+	--Right
+	{"F18C_BORT_NUMBER_ZAK_R_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_ZAK_R_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_ZAK_R_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_ZAK_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_ZAK_R_01", DECAL ,"F18C_bort_number2",true};
+	
+	--Vertical Stab Aircraft Numbers (USN)
+	--Left
+	{"F18C_BORT_NUMBER_KIL_L_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_KIL_L_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_KIL_L_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_L_01", DECAL ,"F18C_bort_number2",true};	
+	--Right
+	{"F18C_BORT_NUMBER_KIL_R_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_100", DECAL ,"F18C_bort_number2",true};
+	{"F18C_BORT_NUMBER_KIL_R_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_10", DECAL ,"F18C_bort_number2",true};	
+	{"F18C_BORT_NUMBER_KIL_R_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_R_01", DECAL ,"F18C_bort_number2",true};
+
+		--Nose Aircraft Numbers (KUWAIT)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_L_01", DECAL ,"empty",true};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_kuw_R_01", DECAL ,"empty",true};		
+	
+
+	--Vertical Stab Numbers (KUWAIT)
+	--Right
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Kuw_L_01", DECAL ,"empty",true};		
+
+	--Nose Aircraft Numbers (FINLAND)
+	--Left
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_L_01", DECAL ,"empty",true};
+	--Right
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_NOSE_fin_R_01", DECAL ,"empty",true};			
+
+	--Vertical Tail Numbers (SWITZERLAND)
+	--Right
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_100", DECAL ,"empty",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", 0 ,"../(v)USN VFA-103 'Jolly Rogers' LINE/F18C_2_DIFF",false};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", ROUGHNESS_METALLIC ,"F18C_2_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_KIL_Switz_L_01", DECAL ,"empty",true};
+
+	--Gear Door Numbers (RAAF)
+	--Right
+	{"F18C_BORT_NUMBER_STV_aus_R_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_STV_aus_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_STV_aus_R_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_STV_aus_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_STV_aus_L_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_STV_aus_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_STV_aus_L_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_STV_aus_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_STV_aus_L_01", DECAL ,"empty",true};		
+		
+	--Aft Fuselage Numbers (RAAF)
+	--Right
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_R_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_R_01", DECAL ,"empty",true};	
+	--Left
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_L_10", DECAL ,"empty",true};	
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", 0 ,"F18C_1_DIFF",false};
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", ROUGHNESS_METALLIC ,"F18C_1_DIF_RoughMet",true};
+	{"F18C_BORT_NUMBER_MTW_aus_L_01", DECAL ,"empty",true};	
+}
+name = "(v)USN VFA-103 'Jolly Rogers' LINE AG-207"
+countries = {"USA",}
+--Property of Digital Coalition Airforce Skinpack
+--By DCAF CTCVisuals 01/06/2018 Inspired by Skate Zilla
+--https://www.fiverr.com/ctcvisuals/create-custom-dcs-world-liveries
