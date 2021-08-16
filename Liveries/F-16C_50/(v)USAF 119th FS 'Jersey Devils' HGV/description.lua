405f963a4072cf301b568c1a680e68273f37d101
CTCGraphics <34329447+CTCGraphics@users.noreply.github.com>
2021-08-16 19:57:01 +0100

    Added 119th FS HGV Scheme and fixed bort number for 2011+

---
+++ Liveries/F-16C_50/(v)USAF 119th FS 'Jersey Devils' HGV/description.lua
@@ -0,0 +1,143 @@
+livery = {
+	
+	--F16_bl50_Kil
+	{"F16_bl50_Kil", DIFFUSE ,"F16_bl50_Kil.dds",false};
+	{"F16_bl50_Kil", ROUGHNESS_METALLIC,"F16_bl50_Kil_RoughMet.dds",false};
+	{"F16_bl50_Kil", NORMAL_MAP,"F16_bl50_Kil_Normal.dds",false};
+	
+	
+	--F16_bl50_Main_1
+	{"F16_bl50_Main_1", DIFFUSE,"F16_bl50_Main_1.dds",false};
+	{"F16_bl50_Main_1", ROUGHNESS_METALLIC,"F16_bl50_Main_1_RoughMet.dds",false};
+	{"F16_bl50_Main_1", NORMAL_MAP,"F16_bl50_Main_1_Normal.dds",false};
+	
+	--F16_bl50_Main_2
+	{"F16_bl50_Main_2", DIFFUSE,"F16_bl50_Main_2",false};
+	{"F16_bl50_Main_2", ROUGHNESS_METALLIC,"F16_bl50_Main_2_RoughMet",false};
+	{"F16_bl50_Main_2", NORMAL_MAP,"F16_bl50_Main_2_Normal",false};
+	
+	--F16_bl50_Main_3
+	{"F16_bl50_Main_3", DIFFUSE ,"F16_bl50_Main_3.dds",false};
+	{"F16_bl50_Main_3", ROUGHNESS_METALLIC,"F16_bl50_Main_3_RoughMet",false};
+	{"F16_bl50_Main_3", NORMAL_MAP,"F16_bl50_Main_3_Normal",false};
+	
+	--F16_bl50_Wing_L
+	{"F16_bl50_wing_L", DIFFUSE,"F16_bl50_wing_L.dds",false};
+	{"F16_bl50_wing_L", ROUGHNESS_METALLIC,"F16_bl50_wing_L_Roughmet.dds",false};
+	{"F16_bl50_wing_L", NORMAL_MAP,"F16_bl50_wing_L_Normal.dds",false};
+	
+	--F16_bl50_Wing_R
+	{"F16_bl50_wing_R", DIFFUSE,"F16_bl50_wing_R.dds",false};
+	{"F16_bl50_wing_R", ROUGHNESS_METALLIC,"F16_bl50_wing_R_Roughmet.dds",false};
+	{"F16_bl50_wing_R", NORMAL_MAP,"F16_bl50_wing_R_Normal.dds",false};
+
+	
+	
+    --F16_bl50_GLASS
+	{"F16_bl50_GLASS", 0 ,"F16_bl50_Glass_Grey",false};
+	{"F16_bl50_GLASS", 13 ,"F16_bl50_Glass_RM",false};
+	{"F16_bl50_GLASS",    DIFFUSE            ,    "F16_bl50_Glass_Grey", false};
+    {"F16bl50__GLASSСLear",    DIFFUSE            ,    "F16_bl50_Glass_Grey", false};
+  
+	--F16_bl50_Gear
+	{"F16_bl50_Gear", DIFFUSE, "f16_bl50_Gear", false};
+	{"F16_bl50_Gear", ROUGHNESS_METALLIC, "f16_bl50_Gear_roughmet", false};
+	
+--F16_bl50_Cockpit_1
+	{"f16c_cockpit_1", DIFFUSE ,"f16c_cockpit_1.dds",false};
+	{"f16c_cockpit_1",	ROUGHNESS_METALLIC			,	"f16c_cockpit_1_Roughmet", false};
+	{"f16c_cockpit_1",	NORMAL_MAP			,	"f16c_cockpit_1_nrm", false};
+	
+	
+	--F16_bl50_Engine
+	{"F16_bl50_Engine", 0 ,"F16_bl50_Engine",false};
+	{"F16_bl50_Engine", ROUGHNESS_METALLIC, "f16_bl50_engine_roughmet", false};
+	{"F16_bl50_Engine",	NORMAL_MAP			,	"f16_bl50_engine_normal", false};
+	
+	--TCTS POD
+	{"ais-pod-t50",	DIFFUSE			,	"ais-pod_t50_diffuse", false};
+	{"ais-pod-t50",	SPECULAR			,	"ais-pod_t50_spec", false};
+--FUEL TANK 370
+	{"Tank_370", DIFFUSE, "fuel_tank_370gal_diff", false};
+	{"Tank_370", ROUGHNESS_METALLIC, "fuel_tank_370gal_diff_RoughMet", false};
+	
+	--F16_bl50_FUEL Tank_300
+	{"PTB_300Gal", 0,	                       	 "Fuel_Tank_300Gal_Tiger",		   false};
+	{"PTB_300Gal", ROUGHNESS_METALLIC,	    "Fuel_Tank_300Gal_Tiger_RoughMet",	    false};	
+
+	--F16_bl50_PILOT+HELMET+PATCH
+	{"pilot_F16", 0, "pilot_F16", false};
+	{"pilot_F16_patch", 0, "pilot_f16_patch", false};
+	{"pilot_F16_patch", ROUGHNESS_METALLIC, "pilot_f16_patch_roughmet.dds", false};
+	{"pilot_F16_patch", NORMAL_MAP, "pilot_f16_patch_nm", false};
+	{"pilot_F16_patch",	 1,	                    "pilot_f16_patch_Tiger_nm",			false};
+	{"f16c_ej_seat", 0, "f16c_ej_seat", false};
+--Pylons--
+	{"LAU_129", DIFFUSE, "lau_129_diff", false};
+	{"LAU_129", ROUGHNESS_METALLIC , "lau_129_diff_RoughMet", false};
+	
+	{"F16_bl50_Wing_Pylon_1", 0, "f16_bl50_wing_pylon_1", false};
+	{"F16_bl50_Wing_Pylon_1", ROUGHNESS_METALLIC , "f16_bl50_wing_pylon_1_RoughMet", false};
+
+
+	{"F16_bl50_Wing_Pylon_2", 0, "f16_bl50_wing_pylon_2", false};
+	{"F16_bl50_Wing_Pylon_2", ROUGHNESS_METALLIC , "f16_bl50_wing_pylon_2_RoughMet", false};
+	{"F16_bl50_Wing_Pylon_2", NORMAL_MAP , "f16_bl50_wing_pylon_2_Normal", false};
+	
+	{"pilot_F16_helmet",	DIFFUSE			,	"pilot_f16_helmet.bmp", false};
+	{"pilot_F16_helmet",	ROUGHNESS_METALLIC		,	"pilot_f16_helmet_rm.bmp", false};
+	{"pilot_F16_helmet",	NORMAL_MAP		,	"pilot_f16_helmet_nm.bmp", false};
+{"MXU-648",	DIFFUSE			,	"MXU-648", false};
+{"MXU-648",	ROUGHNESS_METALLIC			,	"MXU-648_RM", false};
+{"MXU-648",	NORMAL_MAP			,	"MXU-648_NM", false};
+
+	
+	{"F16_bl50_BANO",	DIFFUSE			,	"f16_bl50_BANO.dds", false};
+	
+-- BORT NUMBER--------------------------------------------------------------------------------------------------------------
+
+--F16_bl50_FIN_DECAL	
+    {"F16_bl50_FIN_DECAL",	0,	                "F16_bl50_Kil",				false};
+	{"F16_bl50_FIN_DECAL",	1,	                "F16_bl50_Kil_Normal",		false};
+	{"F16_bl50_FIN_DECAL",	ROUGHNESS_METALLIC,	"F16_bl50_Kil_RoughMet",	false};
+	{"F16_bl50_FIN_DECAL",	DECAL,	            "F16_AF_87_Decal",	false};
+
+--F16_bl50_FIN_BORT_NUMBER
+    {"F16_bl50_FIN_BORT_NUMBER",	0,	                "F16_bl50_Kil",				false};
+	{"F16_bl50_FIN_BORT_NUMBER",	1,	                "F16_bl50_Kil_Normal",		false};
+	{"F16_bl50_FIN_BORT_NUMBER",	ROUGHNESS_METALLIC,	"F16_bl50_Kil_RoughMet",	false};
+	{"F16_bl50_FIN_BORT_NUMBER",	DECAL,	                  "F16_bort_number",	false};
+
+--F16_bl50_NOUSE_DECAL
+    {"F16_bl50_MAT1_BORT_NUMBER_X100",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	1,	                "F16_bl50_Main_1_Normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_Roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_X100",	DECAL,	            "empty",			true};
+
+--F16_bl50_REFUEL_DECAL 
+    {"F16_bl50_MAT1_BORT_NUMBER_DECAL",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	1,	                "F16_bl50_Main_1_Normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_Roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER_DECAL",	DECAL,	            "empty",			true};
+
+--F16_bl50_NOUSE AND REFUEL BORT_NUMBER
+    {"F16_bl50_MAT1_BORT_NUMBER",	0,	                "F16_bl50_Main_1",			false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	1,	                "F16_bl50_Main_1_Normal",	false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	ROUGHNESS_METALLIC,	"F16_bl50_Main_1_Roughmet", false};
+	{"F16_bl50_MAT1_BORT_NUMBER",	DECAL,	            "empty",			true};
+
+--F16_bl50_INTAKE_BORT_NUMBER	
+    {"F16_bl50_INTAKE_BORT_NUMBER",	0,	                "F16_bl50_Main_3",				false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	1,	                "F16_bl50_Main_3_Normal",		false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	ROUGHNESS_METALLIC,	"F16_bl50_Main_3_Roughmet",		false};
+	{"F16_bl50_INTAKE_BORT_NUMBER",	DECAL,	            "empty",			true};
+}
+name = "(v)USAF 119th FS 'Jersey Devils' HGV"
+custom_args =
+{
+[1000] = 0.0, -- change of type of board number (0.0 -default USA, 0.1- )
+[1001] = 1.0, -- vis refuel board number 
+[1002] = 1.0, -- change of type intake board number 
+[1003] = 1.0, -- vis nouse board number 
+} 
+countries = {"USA",}
